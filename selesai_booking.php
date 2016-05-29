a342e50175675b6e80ece73a91f97a2d817e3fc1
Bayu Firmawan Paoh #ganteng <bayupaoh@gmail.com>
2016-05-29 15:48:20 +0700

    j

--- selesai_booking.php
+++ selesai_booking.php
@@ -2,5 +2,5 @@
   $id_user = $_POST["id_user"];
   $kode_unik = $_POST["kode_unik"];
 
-  echo '{"kode":"200","message":"berhhasil"}';
+  echo '{"kode":"200","message":"berhhasil","biaya_parkir":5000}';
 ?>
