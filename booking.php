2a64b4defc7b48fb5e5a6b5c4a918825a2a6c5a4
Bayu Firmawan Paoh #ganteng <bayupaoh@gmail.com>
2016-05-29 15:00:55 +0700

    l

--- booking.php
+++ booking.php
@@ -1,5 +1,7 @@
 <?php
   $id_user = $_POST["id_user"];
+  $id_tempat=$_POST["id_tempat"];
+  $durasi = $_POST["durasi"];
   $waktu_awal=$_POST["waktu_booking"];
   $waktu_akhir=$_POST["waktu_akhir"];
 
