68352d8d127ac692ca0c54f9436c3759b79660f7
unikom codelabs <unikomcodelabs@gmail.com>
2016-06-26 11:38:43 -0700

    fix all

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
+$host = "ap-cdbr-azure-southeast-b.cloudapp.net";
+$user = "b193c8483ad3e7";
+$pass = "27e85c63";
 $database = "smartpark";
 $koneksi = mysql_connect($host,$user,$pass);
 mysql_select_db($database) or die ("Connection failed".mysql_error());
