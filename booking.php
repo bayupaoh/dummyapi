9cb6c83940c2fcc14dbfa494993dd78a01e753a4
Bayu Firmawan Paoh <bayupaoh@gmail.com>
2016-07-21 22:41:26 +0700

    fix booking

--- booking.php
+++ booking.php
@@ -2,9 +2,7 @@
   include "koneksi.php";
   $id_user = $_POST["id_user"];
   $id_tempat=$_POST["id_tempat"];
-  $durasi = $_POST["durasi"];
   $waktu_awal=date("Y/m/d")." ".$_POST["waktu_booking"];
-  $waktu_akhir=date("Y/m/d")." ".$_POST["waktu_akhir"];
   $merk=$_POST["merk"];
   $tipe=$_POST["tipe"];
 
@@ -16,7 +14,7 @@
   	if($data=mysql_fetch_array($hasil)){
   		$kode = $data["kode_voucher"];
   		
- 		$query="insert into transaksi values(null,'$id_user','$kode','$waktu_awal','$waktu_akhir',TIMESTAMPDIFF(HOUR,'$waktu_awal','$waktu_akhir'),TIMESTAMPDIFF(HOUR,'$waktu_awal','$waktu_akhir')*1000,'belum','$merk','$tipe')";
+ 		$query="insert into transaksi values(null,'$id_user','$kode','$waktu_awal','',0,0,'belum','$merk','$tipe')";
 
   		$hasil=mysql_query($query);
   		if($hasil){
